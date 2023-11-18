082e1b31d701773d5aefc8c90f722ed21c19248e
555nb <79388165+555nb@users.noreply.github.com>
2023-11-18 18:52:03 +0800

    Create ut.java

---
+++ src/kjk/ut.java
@@ -0,0 +1,25 @@
+package kjk;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+
+public class ut extends UnitTypes{
+    public static void load(){
+    
+    alpha.speed = 8f;
+    alpha.itemCapacity = 555;
+    alpha.weapons.get(0).bullet.buildingDamageMultiplier = 1.0f;
+
+    beta.speed = 8f;
+    beta.itemCapacity = 555;
+    beta.weapons.get(0).bullet.buildingDamageMultiplier = 1.0f;
+
+    gamma.speed = 8f;
+    gamma.itemCapacity = 555;
+    gamma.weapons.get(0).bullet.buildingDamageMultiplier = 1.0f;
+        
+    }
+    
+}
