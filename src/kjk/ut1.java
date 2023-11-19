2b6a99286d761f3d74475ffd8fa8b1687e67af18
555nb <79388165+555nb@users.noreply.github.com>
2023-11-19 12:31:20 +0800

    Update ut1.java

--- src/kjk/ut1.java
+++ src/kjk/ut1.java
@@ -5,23 +5,19 @@ import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
 
-public class ut1 extends Blocks {
-    @Override
-    public static void load() {
-        conveyor = new Conveyor("conveyor") {{
-            requirements(Category.distribution, with(Items.copper, 1));
-            health = 45;
-            speed = 0.03f * 2;                  // 修改speed的值
-            displayedSpeed = 4.2f * 2;          // 修改displayedSpeed的值
-            buildCostMultiplier = 2f;
-            researchCost = with(Items.copper, 5);
-        }};
+public class ut1 extends Blocks{
+    public static void load(){
 
-        titaniumConveyor = new Conveyor("titanium-conveyor") {{
-            requirements(Category.distribution, with(Items.copper, 1, Items.lead, 1, Items.titanium, 1));
-            health = 65;
-            speed = 0.08f * 2;                  // 修改speed的值
-            displayedSpeed = 11f * 2;           // 修改displayedSpeed的值
-        }};
+    public static Blocks conveyor,titaniumConveyor;
+    
+    conveyor.displayedSpeed *= 2;
+    conveyor.speed *= 2;
+    
+    titaniumConveyor.displayedSpeed *= 2;
+    titaniumConveyor.speed *= 2;
+
+    
+        
     }
+    
 }
