0113e79a9015327267f49dd6cf2ac0a0eaae600c
555nb <79388165+555nb@users.noreply.github.com>
2023-11-19 11:42:38 +0800

    Add files via upload

---
+++ src/kjk/ut1.java
@@ -0,0 +1,22 @@
+package kjk;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+
+public class ut1 extends Blocks{
+    public static void load(){
+    
+    super.load();
+    conveyor.displayedSpeed *= 2;
+    conveyor.speed *= 2;
+    
+    titaniumConveyor.displayedSpeed *= 2;
+    titaniumConveyor.speed *= 2;
+
+    
+        
+    }
+    
+}
