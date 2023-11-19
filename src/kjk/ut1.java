31c3f9ad0a032b8c985c667ac171b6ef19e9751f
555nb <79388165+555nb@users.noreply.github.com>
2023-11-19 11:55:10 +0800

    Update ut1.java

--- src/kjk/ut1.java
+++ src/kjk/ut1.java
@@ -8,8 +8,6 @@ import mindustry.content.*;
 public class ut1 extends Blocks{
     public static void load(){
     
-    conveyor.speed = 0.06f;
-    titaniumConveyor.speed = 0.16f;
 
     
         
