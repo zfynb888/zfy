e4b5b2058608cb84c995ed6ea0f84a515bb3dc43
555nb <79388165+555nb@users.noreply.github.com>
2023-11-19 10:48:23 +0800

    Update ut.java

--- src/kjk/ut.java
+++ src/kjk/ut.java
@@ -10,20 +10,26 @@ public class ut extends UnitTypes{
 
     alpha.health = 300f;
     alpha.itemCapacity = 555;
+    alpha.mineSpeed = 13f;
     alpha.buildSpeed = 1f;
     alpha.drag = 0.1f;
+    alpha.weapons.get(0).bullet.speed = 5f;
     alpha.weapons.get(0).bullet.buildingDamageMultiplier = 0.2f;
 
     beta.health = 340f;
     beta.itemCapacity = 555;
+    beta.mineSpeed = 14f;
     beta.buildSpeed = 1.5f;
     beta.drag = 0.1f;
+    alpha.weapons.get(0).bullet.speed = 6f;
     beta.weapons.get(0).bullet.buildingDamageMultiplier = 0.2f;
 
     gamma.health = 440f;
     gamma.itemCapacity = 555;
+    gamma.mineSpeed = 16f;
     gamma.buildSpeed = 2f;
     gamma.drag = 0.1f;
+    alpha.weapons.get(0).bullet.speed = 7f;
     gamma.weapons.get(0).bullet.buildingDamageMultiplier = 0.2f;
         
     }
