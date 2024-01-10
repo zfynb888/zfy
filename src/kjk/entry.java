1668991ce70b4ae8e61302cc82ef0b769efe31e5
zfynb888 <156160149+zfynb888@users.noreply.github.com>
2024-01-10 13:08:38 +0800

    Update entry.java

--- src/kjk/entry.java
+++ src/kjk/entry.java
@@ -12,7 +12,20 @@ import mindustry.ui.dialogs.*;
 public class entry extends Mod{
 
     public entry(){
-        
+        Log.info("Loaded ExampleJavaMod constructor.");
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("kjk");
+                dialog.cont.add("kjk").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'kjk-test-mod' in its config)
+                dialog.cont.image(Core.atlas.find("kjk-test-mod-frog")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
     }
 
     @Override
